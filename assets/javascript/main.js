93162f91a1cb560df78f9495e66abad955748611
Akash-Dutta-Dev-io <149801749+Akash-Dutta-Dev-io@users.noreply.github.com>
2023-12-15 03:11:59 +0530

    new
    added mail box

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -17,3 +17,18 @@ document.querySelectorAll(".nav-links").forEach(n => n.addEventListener("click",
   hamburger.classList.remove("active");
   nav.classList.remove("active");
 }))
+
+
+function sendEmail(){
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "akashdutta.business96@gmail.com",
+    Password : "3557A9EADA38E62CA92D4344005EACB43C8D",
+    To : 'akashdutta.business96@gmail.com',
+    From : document.getElementById("email").value,
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+}
